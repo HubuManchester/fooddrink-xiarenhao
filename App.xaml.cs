2e795e61b47c7c6eec5bf0e2a449306f95754a7e
xiarenhao <2066407983@qq.com>
2026-05-31 17:56:31 +0800

    Add files via upload

---
+++ App.xaml.cs
@@ -0,0 +1,20 @@
+﻿using BiteRecord.Services;
+using BiteRecord.Views;
+
+namespace BiteRecord;
+
+public partial class App : Application
+{
+    public static DatabaseService Database { get; private set; }
+
+    public App()
+    {
+        InitializeComponent();
+
+        // Initialize database
+        Database = new DatabaseService();
+        Database.InitializeDatabase();
+
+        MainPage = new AppShell();
+    }
+}
---
+++ AppShell.xaml.cs
@@ -0,0 +1,17 @@
+﻿using BiteRecord.Views;
+
+namespace BiteRecord;
+
+public partial class AppShell : Shell
+{
+    public AppShell()
+    {
+        InitializeComponent();
+
+        // 注册路由
+        Routing.RegisterRoute(nameof(AddItemPage), typeof(AddItemPage));
+        Routing.RegisterRoute(nameof(MainPage), typeof(MainPage));
+        Routing.RegisterRoute(nameof(HardwarePage), typeof(HardwarePage));
+        Routing.RegisterRoute(nameof(SettingsPage), typeof(SettingsPage));
+    }
+}
